bd38fd8e9c48c092fbcc96c2b7de6aead2fbb6b3
programmer325-jetlearner <asyedtanim1@gmail.com>
2025-05-18 11:09:32 +0100

    Add files via upload

--- coding.py
+++ coding.py
@@ -8,10 +8,12 @@ money_man=Actor("money_man")
 money_man.pos=300,300
 
 money=Actor("money") 
+
 money.pos=350,350
 
 
 dollars=0
+game_over=False
 
 def draw():
    screen.clear()
@@ -20,6 +22,16 @@ def draw():
    money.draw()
 
    screen.draw.text(f"dollars: {dollars}",color="blue",topleft=(10,10))
+
+   if game_over:
+      screen.clear()
+      screen.fill("blue")
+      screen.draw.text(f"Tax collecting season is over! The amount of money you got was {dollars}",midtop=(WIDTH/2,10),fontsize=20,color="yellow")
+
+
+def time_up():
+    global game_over
+    game_over=True
 
 
 def place_money():
@@ -42,6 +54,10 @@ def update():
       place_money()
       dollars+=2000
 
+
+clock.schedule(time_up,30.0)
+
+        
 
 
 
